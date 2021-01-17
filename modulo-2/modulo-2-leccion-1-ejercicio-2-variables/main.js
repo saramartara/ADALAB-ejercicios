18e2760fbee844f2fc9be0633305ab88df1673ec
saramartara <saramartinezaragon@gmail.com>
2021-01-17 12:58:21 +0100

    add module-2-lesson-1-exer-3-helloWorld

--- modulo-2/modulo-2-leccion-1-ejercicio-2-variables/main.js
+++ modulo-2/modulo-2-leccion-1-ejercicio-2-variables/main.js
@@ -2,4 +2,3 @@
 document.querySelector('h1').innerHTML = 'Variables';
 let address = 'Los Puertas 23, Benajarafe, Málaga';
 address = 'Los Puertas 25';
-
--- modulo-2/modulo-2-leccion-1/main.js
+++ modulo-2/modulo-2-leccion-1/main.js
@@ -7,3 +7,16 @@ document.querySelector('h1').innerHTML = '¡Hola mundo!';
 let address = "Los Puertas 23";
 
 address = "Venezuela 16";
+
+// const mainTitle = document.querySelector('h1'); /*selector de ELEMENTO (etiqueta)*/
+// const mainTitle = document.querySelector('#maintTitle'); /*selector de ID*/
+// const mainTitle = document.querySelector('.mainTitle'); /*selector de CLASE*/
+
+// Obtenemos el elemento con el que queremos trabajar usando document.querySelector()
+const titleElement = document.querySelector('.title');
+
+// Cambiamos su contenido con innerHTML
+titleElement.innerHTML = 'Bienvenida';
+
+// Cambiamos el contenido del elemento, indicando que sea igual al actual, más una nueva palabra añadida
+titleElement.innerHTML = titleElement.innerHTML + ' adalaber';
