d01897726157d0244099ad9d649c0359a724303a
saramartara <saramartinezaragon@gmail.com>
2021-01-17 12:31:19 +0100

    add module-2-lesson-1-exer-2-fixing mistakes

---
+++ modulo-2/modulo-2-leccion-1-ejercicio-2-variables/main.js
@@ -0,0 +1,5 @@
+"use strict";
+document.querySelector('h1').innerHTML = 'Variables';
+let address = 'Los Puertas 23, Benajarafe, Málaga';
+address = 'Los Puertas 25';
+
---
+++ modulo-2/modulo-2-leccion-1/main.js
@@ -0,0 +1,9 @@
+
+
+'use strict';
+
+document.querySelector('h1').innerHTML = '¡Hola mundo!';
+
+let address = "Los Puertas 23";
+
+address = "Venezuela 16";
