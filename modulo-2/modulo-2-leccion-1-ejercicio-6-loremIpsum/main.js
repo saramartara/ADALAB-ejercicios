956b8ba9b3f5bf5ca180cd6b607df95e95edfb9c
saramartara <saramartinezaragon@gmail.com>
2021-01-18 18:13:41 +0100

    add module2-lesson1-exer6-loremIpsum

---
+++ modulo-2/modulo-2-leccion-1-ejercicio-6-loremIpsum/main.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const divElement = document.querySelector('.container');
+
+const content = '<h1>Lorem Ipsum</h1><img src= "http://via.placeholder.com/350x150"/> <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>';
+
+divElement.innerHTML = content;
