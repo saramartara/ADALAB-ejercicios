f102f0a4034ba2de1da3aea3e267797f8e3a20a2
saramartara <saramartinezaragon@gmail.com>
2021-01-19 21:57:43 +0100

    add module2-lesson2-exer2-DayanaMola

---
+++ modulo-2/modulo-2-leccion-2-ejercicio-3-comprandoFrutería/javascript/main.js
@@ -0,0 +1,20 @@
+'use strict'
+const kilosKiwis = 2;
+const precioKiwis = 5;
+
+let kiwis = kilosKiwis * precioKiwis;
+
+const kilosPeras = 3;
+const precioPeras = 2;
+
+let peras = kilosPeras * precioPeras;
+
+const kilosUvas = 0.5;
+const precioUvas = 4;
+
+let uvas = kilosUvas * precioUvas;
+
+let totalCompra = kiwis + peras + uvas;
+
+// console.log(totalCompra);
+
