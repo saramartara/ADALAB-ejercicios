353e6d9ffe2e5c6bd0f09a84c016436e30acf120
saramartara <saramartinezaragon@gmail.com>
2021-01-20 11:14:59 +0100

    add module2-lesson2-exer5-lifeHours

--- modulo-2/modulo-2-leccion-2-ejercicio-4-cenaNavidad/javascript/main.js
+++ modulo-2/modulo-2-leccion-2-ejercicio-4-cenaNavidad/javascript/main.js
@@ -10,3 +10,11 @@ const aPagarPorAna = aPagarCadaUna + chupitoDeAna;
 console.log(aPagarCadaUna);
 console.log(aPagar);
 
+console.log('Todas tienen que pagar' + aPagarCadaUna + '€');
+console.log('Peeeero, Ana tieen que pagar ' + aPagarPorAna + '€');
+
+/*Necesita los paréntesis en la operación para que lo entienda como number y no string*/
+console.log('Operación mat:' + (2 + 2));
+
+/*aquí no hace falta porque realiza primero la suma*/
+console.log(2 + 2 + 'es un buen número');
---
+++ modulo-2/modulo-2-leccion-2-ejercicio-5-horasVividas/main.js
@@ -0,0 +1,8 @@
+'use strict'
+
+const myAge = 39;
+const hoursByYear = 24 * 365; 
+
+const result = myAge * hoursByYear;
+
+console.log(result)
