422b0641703d445c79b62f56ff5f9229ef08cc98
saramartara <saramartinezaragon@gmail.com>
2021-01-20 09:34:30 +0100

    add module2-lesson2-exer4-christmasDinner

---
+++ modulo-2/modulo-2-leccion-2-ejercicio-4-cenaNavidad/javascript/main.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const aPagar = 128;
+const numComensales = 9;
+const chupitoDeAna = 2;
+
+const aPagarCadaUna = (aPagar - chupitoDeAna) / numComensales;
+const aPagarPorAna = aPagarCadaUna + chupitoDeAna;
+
+console.log(aPagarCadaUna);
+console.log(aPagar);
+
