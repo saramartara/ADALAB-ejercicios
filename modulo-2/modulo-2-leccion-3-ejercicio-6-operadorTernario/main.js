7b2cb502f4adfb9ba9d651c66fe1de0055ba556c
saramartara <saramartinezaragon@gmail.com>
2021-01-23 22:31:32 +0100

    add module2-lesson3-exer6

---
+++ modulo-2/modulo-2-leccion-3-ejercicio-6-operadorTernario/main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+let avocados;
+const avocadoPrice = 1.5;
+const money = 43;
+
+// if (money >= avocadoPrice) {
+//   avocados = money / avocadoPrice;
+// } else {
+//   avocados = 0;
+// }
+
+
+money >= avocadoPrice ? avocados = money / avocadoPrice : avocados = 0;
+console.log(avocados);
