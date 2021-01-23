e7cfbc2420ad8e02833dc35341a1138b5743f2ae
saramartara <saramartinezaragon@gmail.com>
2021-01-23 20:54:24 +0100

    add module2-lesson2-exer6&7

---
+++ modulo-2/modulo-2-leccion-2-ejercicio-6-holaAdalaber/main.js
@@ -0,0 +1,7 @@
+'use strict'
+
+
+const namee = 'Lola';
+
+document.querySelector ('.hi').innerHTML = 'Hola ' + namee + ', encantada de conocerte.';
+console.log('Hola ' + namee + ', encantada de conocerte.');
---
+++ modulo-2/modulo-2-leccion-2-ejercicio-7-holaAdalaber2/main.js
@@ -0,0 +1,7 @@
+'use strict'
+
+
+const namee = 'Carmen';
+
+document.querySelector ('.hi').innerHTML = (`Hola ${namee} encantada de conocerte.`);
+console.log(`Hola ${namee} encantada de conocerte.`);
