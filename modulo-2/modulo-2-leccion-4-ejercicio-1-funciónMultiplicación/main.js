5e07586e785f764fb7730a6a2c36b8dfb3a5d9f9
saramartara <saramartinezaragon@gmail.com>
2021-01-23 22:39:20 +0100

    add module2-lesson4-exer1

---
+++ modulo-2/modulo-2-leccion-4-ejercicio-1-funciónMultiplicación/main.js
@@ -0,0 +1,8 @@
+'use strict';
+
+function multiplication (a, b) {
+    return a * b;
+}
+console.log(multiplication (2, 5));
+console.log(multiplication (6, 7));
+console.log(multiplication (9, 4));
