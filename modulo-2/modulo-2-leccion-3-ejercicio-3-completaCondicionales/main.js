cc58cd341dcf1154af581e1dfe8d338ebfcdd24e
saramartara <saramartinezaragon@gmail.com>
2021-01-23 22:21:06 +0100

    add module2-lesson3-exer3

---
+++ modulo-2/modulo-2-leccion-3-ejercicio-3-completaCondicionales/main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+let a = 58;
+
+if (a == 0) {
+    console.log('El número es 0')
+  } else if ( a < 0) {
+    console.log('El número es negativo')
+  } else if ( (a => 12 ) && (a <= 20) ) {
+    console.log('El número más 2 es mayor que 13 pero menor o igual que 20')
+  } else if ( a > 20 && a < 50) {
+    console.log('El número es mayor que 20 pero menor que 50')
+  } else {
+    console.log('el número no es 123123125')
+  }
