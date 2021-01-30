1d3e3373ee5d0b71b88bc9a60a74372d7b08032d
saramartara <saramartinezaragon@gmail.com>
2021-01-30 10:25:09 +0100

    update exercise 2-5.1 with arrow function

--- modulo-2/modulo-2-leccion-5-ejercicio-1-holaclick/main.js
+++ modulo-2/modulo-2-leccion-5-ejercicio-1-holaclick/main.js
@@ -1,16 +1,21 @@
 'use strict';
 
-
 // elemento de HTML
 const button = document.querySelector('.btn');
 let text = document.querySelector('.text');
 
 // handler
-// function changeText() {
-//   return text.innerHTML = `Mi primer click, ¡ole yo y la mujer que me parió!`;
-// }
-
-function changeText() => {text.innerHTML = `Prueba`}
+function changeText() {
+  return (text.innerHTML = `Mi primer click, ¡ole yo y la mujer que me parió!`);
+}
 
 // listener sobre el elemento, con tipo de evento y handler
 button.addEventListener('click', changeText);
+
+// misma función en arrow con mouseover
+
+const changeTextArrow = () => {
+  text.innerHTML = `Prueba arrow function`;
+};
+
+button.addEventListener('mouseover', changeTextArrow);
