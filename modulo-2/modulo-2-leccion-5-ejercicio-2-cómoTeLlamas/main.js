3a1a7df2efa9a82447802501bf979a78ef4fd022
saramartara <saramartinezaragon@gmail.com>
2021-01-30 11:49:57 +0100

    add exercise 2-5.2

--- modulo-2/modulo-2-leccion-5-ejercicio-2-cómoTeLlamas/main.js
+++ modulo-2/modulo-2-leccion-5-ejercicio-2-cómoTeLlamas/main.js
@@ -1,62 +1,19 @@
 'use strict';
-//input data: 
-//- name input .js-name (value)
 
-//output data:
-// Hola + .js-name(value) span.js-result (innerHTML)
+//input data: name input .js name (value)
+//output data: Hola + .js-name(value) into span .js-result (innerHTML)
 
-// Steps
-// 1. Get name
+const buttonElement = document.querySelector('.js-btn');
+const nameElement = document.querySelector('.js-name');
+const resultElement = document.querySelector('.js-result');
 
-function getName() {
-  // get element from HTML
+//steps:
+//1. Get name
+//2. Render name
 
-  const nameElement = document.querySelector("js-name");
-  // extract name from value
-
-  const name = nameElement.value;
-
-  // return name
-
-  return name;
+function renderName() {
+  let nameValue = nameElement.value;
+  return (resultElement.innerHTML = `Hola ${nameValue} `);
 }
 
-
-// 2. Compose sentence
-// Input data:
-// Hola + name
-// output data: 
-// Hola name
-
-function composeSentence (a){
-  const final = 'Hola ' + a;
-  return finalSentence;
-}
-
-
-// 3. Render Hola name
-
-function renderName(b) {
-  const spanResultElement = document.querySelector(".js-result");
-
-  spanResultElement.innerHTML = `Hola${b}`;
-
-}
-
-
-const name = getName();
-const sencente = composeSentence(name);
-renderName(name);
-
-// // elemento de HTML
-// const button = document.querySelector('.btn');
-// let nameText = document.querySelector('.input').value;
-
-// // handler
-// function hi() {
-//   console.log ('hola ' + nameText);
-// }
-
-// listener sobre el elemento, con tipo de evento y handler
-
-renderName.addEventListener('click', renderName);
+buttonElement.addEventListener('click', renderName);
