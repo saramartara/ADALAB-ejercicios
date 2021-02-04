96cd8ad832299cc91bb08eaa03d3c129ddbb981f
saramartara <saramartinezaragon@gmail.com>
2021-02-04 22:02:01 +0100

    add exercise 2- 13.7

--- modulo-2/modulo-2-leccion-13-ejercicio-7 - reduce/main.js
+++ modulo-2/modulo-2-leccion-13-ejercicio-7 - reduce/main.js
@@ -1,31 +1,10 @@
 'use strict';
 
-console.log('13: ejercicio 4');
+console.log('13: ejercicio 7');
 
-const users = [
-  { name: 'María', isPremium: false },
-  { name: 'Lucía', isPremium: true },
-  { name: 'Susana', isPremium: true },
-  { name: 'Rocío', isPremium: false },
-  { name: 'Inmaculada', isPremium: false },
-];
+const times = [56, 9, 45, 28, 35];
 
-const premiumUsers = users.filter((user) => user.isPremium === true);
-console.log(premiumUsers);
-
-// const greetings = users.map(function (user) {
-//   if (user.isPremium) {
-//     console.log(`Bienvenida ${user.name}. Gracias por confiar en nosotros.`);
-//   } else {
-//     console.log(`Bienvenida ${user.name}`);
-//   }
-// });
-
-// console.log(greetings);
-
-// const names = ['María', 'Lucía', 'Susana', 'Rocío', 'Inmaculada'];
-// console.log(names);
-
-// const greetings = names.map((name) => `Bienvenida  ${name}`);
-
-// console.log(greetings);
+const average = times.reduce((acc, time) => acc + time);
+console.log(average);
+const result = average / times.length;
+console.log(result);
