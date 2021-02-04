ac0af136660c62dba3f27323b650dd29f8e8ad81
saramartara <saramartinezaragon@gmail.com>
2021-02-04 21:04:28 +0100

    add exercise 2-13.2

---
+++ modulo-2/modulo-2-leccion-13-ejercicio-2/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+console.log('13: ejercicio 2');
+
+const names = ['María', 'Lucía', 'Susana', 'Rocío', 'Inmaculada'];
+console.log(names);
+
+const greetings = names.map((name) => `Bienvenida  ${name}`);
+
+console.log(greetings);
