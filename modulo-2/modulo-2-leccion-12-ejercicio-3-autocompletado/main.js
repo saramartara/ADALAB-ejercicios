101e091456a20ea6379660d5216f6939dc6773c2
saramartara <saramartinezaragon@gmail.com>
2021-02-06 14:54:59 +0100

    add exercise 2- 4.7 solved

--- modulo-2/modulo-2-leccion-12-ejercicio-3-autocompletado/main.js
+++ modulo-2/modulo-2-leccion-12-ejercicio-3-autocompletado/main.js
@@ -1,22 +1,40 @@
 'use strict';
 
-console.log('holi');
+//Lección 12 - Ejercicio 3
 
-//Lección 11 - Ejercicio 2
+const arrayData = [
+  { name: 'Nadia', surname: 'Murias', phone: 123456789 },
+  { name: 'Sara', surname: 'Martínez', phone: 656952805 },
+  { name: 'Maricarmen', surname: 'López', phone: 987654321 },
+];
 
-// const arrayData = [
-//   { name: 'Nadia', surname: 'Murias', phone: 123456789 },
-//   { name: 'Sara', surname: 'Martínez', phone: 656952805 },
-//   { name: 'Maricarmen', surname: 'López', phone: 987654321 },
-// ];
-
-const element = document.querySelector('.title');
-console.log(element);
+const selectionElement = document.querySelector('.auto');
+const surnameElement = document.querySelector('.item2');
+const phoneElement = document.querySelector('.item3');
 
 // función
 
-// function handleSelect() {}
+function handleSelect() {
+  for (let i = 0; i < arrayData.length; i++) {
+    if (i === 0) {
+      surnameElement.value = arrayData[i].surname;
+      phoneElement.value = arrayData[i].phone;
+    } else if (i === 1) {
+      surnameElement.value = arrayData[i].surname;
+      phoneElement.value = arrayData[i].phone;
+    } else {
+      surnameElement.value = arrayData[i].surname;
+      phoneElement.value = arrayData[i].phone;
+    }
+  }
+  handleSelect();
+
+  //   if (arrayData.name === 'Nadia') {
+  //     surnameElement.value = 'Murias';
+  //     phoneElement.value = '123456789';
+  //   }
+}
 
 // CUANDO la usuaria cambie el select hacemos handleSelect
 
-// selectionElement.addEventListener('change', handleSelect);
+selectionElement.addEventListener('change', handleSelect);
--- modulo-2/modulo-2-leccion-4-ejercicio-7-combinandoFunciones/main.js
+++ modulo-2/modulo-2-leccion-4-ejercicio-7-combinandoFunciones/main.js
@@ -1,13 +1,11 @@
 'use strict';
 
-//con función 1 acceder al párrafo .paragraph y recoger su valor con .innerHTML
-
 function getEl(element) {
   const htmlEl = document.querySelector(element);
   if (htmlEl !== null) {
     const htmlElValue = htmlEl.innerHTML;
     const numberValue = parseInt(htmlElValue);
-    console.log(numberValue);
+    oddNumber(numberValue);
   } else {
     console.log(
       `No existe ningún elemento con clase, id o tag llamado ${htmlEl}`
@@ -15,23 +13,12 @@ function getEl(element) {
   }
 }
 
-//convertir el valor a número .parseInt
 const paragraphElement = getEl('.paragraph');
 
-// let paragraphValue = paragraphElement.innerHTML;
-// console.log(paragraphValue);
-// const numberValue = parseInt(paragraphValue);
-// console.log(numberValue);
-
-// // //con la función 2:
-// // // si es par escribir en consola: 'Este número es PAR: ...' o 'Este número es IMPAR: ...'
-
-// function oddNumber(a) {
-//   if (a % 2 == 0) {
-//     console.log(`Este número es PAR: ${a}`);
-//   } else {
-//     console.log(`Este número es IMPAR: ${numberValue}`);
-//   }
-// }
-
-// console.log(oddNumber(numberValue));
+function oddNumber(number) {
+  if (a % 2 == 0) {
+    console.log(`Este número es PAR: ${number}`);
+  } else {
+    console.log(`Este número es IMPAR: ${number}`);
+  }
+}
